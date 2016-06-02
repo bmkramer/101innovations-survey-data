02d765880a5c874c959077b3370526f921bfd19b
Chris van Run <Chris.vanrun@gmail.com>
2016-06-02 12:43:42 +0200

    Gave up. Fuzzy match makes to much errors. Man handle this. There are only 120ish entries.

--- linkdb.py
+++ linkdb.py
@@ -4,10 +4,14 @@
 # Cleans the database.
 #
 #
+# Note: requires the fuzzywuzzy python library
 ## Chris van Run, Mozilla sprint, 2 juni 2016
 
 import csv
+import re
 
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 def read_tool_db():
 	dict = {}
@@ -23,12 +27,35 @@ def read_variable_db():
 	with open('variable_db.csv', 'rt') as csvfile:
 	    csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
 	    next(csvreader, None)  # skip the headers
-	    for collumn_name, tool_name in csvreader:
-	        dict[tool_name] = collumn_name
+	    for column_name, tool_name in csvreader:
+	    	tool_name = tool_name[tool_name.find('-')+1:]
+	        dict[tool_name] = column_name
 	return dict
+
+def clean_db(database):
+	new_database = {}
+	for key in database.keys():
+		new_database[clean_key(key)] = database[key]
+	return new_database;
+
+def clean_key(key):
+	new_key = key.strip()
+	new_key = new_key.lower()
+	new_key = new_key.replace(' ','')
+	return new_key
 
 toolDB = read_tool_db()
 variableDB = read_variable_db()
 
-print toolDB
-print variableDB
+cleaned_variableDB = clean_db(variableDB)
+cleaned_toolDB = clean_db(toolDB)
+
+index = 1
+for key_toolDB in cleaned_toolDB.keys():
+	for key_varDB in cleaned_variableDB:
+		match_ratio = fuzz.ratio(key_varDB, key_toolDB)
+		if match_ratio> 70:
+			print index,": [",match_ratio,"  ]","\t\t",key_varDB,"\t<=>\t",key_toolDB
+			index = index+1
+
+
